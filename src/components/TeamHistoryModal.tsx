b4d2980861a41a3b7caccec5a36167750cbf4a93
unknown <zhou-qiaofeng@siac.co.jp>
2026-06-22 16:11:49 +0900

    auto: update TeamHistoryModal.tsx

--- src/components/TeamHistoryModal.tsx
+++ src/components/TeamHistoryModal.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { getFlagUrl, getTeamDisplay } from '@/lib/flags'
 
